2d4ad88094b1694a00c8bfb2c5085bb091f73cab
BotkaB <botikabinaka@gmail.com>
2025-01-11 09:15:03 +0100

    kepfeltoltes backend órai

---
+++ kepfeltoltes_backend/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('file-upload', [KepekController::class, 'index'])->name('file.upload');
+Route::post('file-upload', [KepekController::class, 'store'])->name('file.upload.store');
